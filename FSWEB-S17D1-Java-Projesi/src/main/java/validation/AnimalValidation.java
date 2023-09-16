c70117f927f7b8df9f81a59078f196e4e24a5f63
Canberk Ok <canberk.ok@gmail.com>
2023-09-16 16:03:32 +0300

    Project OK

---
+++ FSWEB-S17D1-Java-Projesi/src/main/java/validation/AnimalValidation.java
@@ -0,0 +1,20 @@
+package validation;
+
+import Entity.Animal;
+
+import java.util.Map;
+
+public class AnimalValidation {
+
+    public static boolean isIdValid (int id ) {
+     return  !( id<0 );
+    }
+    public static  boolean isMapContainsKey(Map<Integer, Animal> animals, int id ) {
+        return animals.containsKey(id);
+    }
+
+    public static boolean isAnimalCredentialsValid(Animal animal) {
+        return !(animal.getId()<=0 || animal.getName() == null || animal.getName().isEmpty() );
+    }
+
+}
